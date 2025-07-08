13884e27b6e89a7c3bb66638bc91c64e5b6bd166
Everton Albuquerque <186333879+evertonalb@users.noreply.github.com>
2025-07-08 18:41:41 -0300

    Assign values to vertices and indices arrays in Polygon constructor

--- src/polygon.cpp
+++ src/polygon.cpp
@@ -3,9 +3,25 @@
 
 Polygon::Polygon(){}
 
-Polygon::Polygon(int n, float radius) : n(n) {
+Polygon::Polygon(int n, float radius) : n(n), numIndices(3*(n-2)) {
 	vertices = new SDL_Vertex[n];
-	indices = new int[(n-2) * 3];
+	indices = new int[3*(n-2)];
+
+	// Vertices
+	Vector v(radius, 0);
+	float angle = 360.0 / n;
+	for (int i = 0; i < n; i++){
+		vertices[i].position = v;
+		v.rotate(angle);
+	}
+
+	// Indices
+	for (int i = 0; i < n-2; i++){
+		indices[3*i + 0] = 0;
+		indices[3*i + 1] = i + 1;
+		indices[3*i + 2] = i + 2;
+	}
+	
 }
 
 Polygon::~Polygon(){
